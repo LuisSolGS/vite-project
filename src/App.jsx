0dc129130e5bef2b579b5e127c278a11e37ecca9
LuisSolGS <167305924+LuisSolGS@users.noreply.github.com>
2025-01-10 16:28:31 -0300

    Add option to filter by platform

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ export default function App() {
   const [entry, setEntry] = useState('loading');
   let list = [];
 
-  if (entry == 'loading'){
-    axios.get('http://127.0.0.1:8000/members/')
+  async function getListTopGames(platform = 'all'){
+    axios.get('http://127.0.0.1:8000/members/?platform=' + platform)
     .then(function (res) {
-      console.log(res.data);
+      //console.log(res.data);
       for (let i = 0; i < res.data.length; i++){
         list.push({key : res.data[i].id, img : `https://images.igdb.com/igdb/image/upload/t_cover_big/${res.data[i].cover.image_id}.jpg`, title: res.data[i].name, text : res.data[i].summary})
       }
@@ -28,11 +28,24 @@ export default function App() {
     });
   }
 
+  if (entry == 'loading'){
+    getListTopGames()
+  }
+
   return (
         <>
           <Header />
+          <header>
+            <nav>
+                <a href="#" onClick={() => getListTopGames('all')}>all</a>
+                <a href="#" onClick={() => getListTopGames('PC')}>PC</a>
+                <a href="#" onClick={() => getListTopGames('PlayStation')}>PlayStation</a>
+                <a href="#" onClick={() => getListTopGames('Xbox')}>Xbox</a>
+                <a href="#" onClick={() => getListTopGames('Switch')}>Nintendo Switch</a>
+            </nav>
+          </header>
           <main className="container">
-              {entry}
+              {entry === 'loading' ? <h1>Loading</h1> : entry}
           </main>
         </>
     )  
--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header>
-            <h1>List of games.</h1>
+            <h1>List of Games</h1>
         </header>
     )
 }
